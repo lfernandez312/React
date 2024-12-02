41bfa81aafe621478ae4ba404044104f13f0dbb1
Leonel <lfernandez0312@gmail.com>
2024-12-02 20:37:12 -0300

    Agregado CartWidget, ItemListContainer y cambios en NavBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import NavBar from './components/NavBar'; // Importo NavBar
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Configuración de rutas
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <Router>
       <div>
-        {/* Incluimos el componente NavBar */}
+        {/* Incluyo el componente NavBar */}
         <NavBar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<h2>Bienvenido a Mi Tienda</h2>} />
+            <Route path="/" element={<ItemListContainer greeting="Bienvenido a Mi Tienda"> {/*contenido a children */} 
+              <p>Disfruta de nuestras ofertas.</p> 
+            </ItemListContainer>} />
             <Route path="/ropa" element={<h2>Ropa</h2>} />
             <Route path="/electronica" element={<h2>Electrónica</h2>} />
             <Route path="/hogar" element={<h2>Hogar</h2>} />
